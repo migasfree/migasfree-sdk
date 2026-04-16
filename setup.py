2588cabe8b81227e99118dd3aba956a2566389b8
Jose Antonio Chavarría <jachavar@gmail.com>
2026-04-16 15:01:38 +0200

    refactor(setup): migrate to setuptools
    - Replaced distutils with setuptools for modern packaging support.
    - Updated build command to build_py.
    - Improved compatibility with automated build tools (stdeb, bdist_rpm).

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2018-2020 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2018-2026 Jose Antonio Chavarría <jachavar@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,50 +15,64 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import os
-import sys
 import glob
+import os
 import subprocess
+import sys
 
-from distutils.core import setup
-from distutils.command.build import build
-from distutils.command.install_data import install_data
-from distutils.log import info, error
-from distutils.dep_util import newer
+from setuptools import setup
+from setuptools.command.build_py import build_py
+
+try:
+    from setuptools.command.install_data import install_data
+except ImportError:
+    from distutils.command.install_data import install_data
+from distutils.log import error, info
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-README = open(os.path.join(PATH, 'README.md')).read()
-VERSION = open(os.path.join(PATH, 'VERSION')).read().strip()
-APP_NAME = 'migasfree-sdk'
+README = open(os.path.join(PATH, "README.md")).read()
+VERSION = open(os.path.join(PATH, "VERSION")).read().strip()
+APP_NAME = "migasfree-sdk"
 
-PO_DIR = 'i18n'
-MO_DIR = os.path.join('build', 'mo')
+PO_DIR = "i18n"
+MO_DIR = os.path.join("build", "mo")
 
 
-class BuildData(build):
+def newer(source, target):
+    """Check if source is newer than target."""
+    try:
+        return os.path.getmtime(source) > os.path.getmtime(target)
+    except OSError:
+        return True
+
+
+class BuildData(build_py):
     def run(self):
-        build.run(self)
-
-        for po in glob.glob(os.path.join(PO_DIR, '*.po')):
+        for po in glob.glob(os.path.join(PO_DIR, "*.po")):
             lang = os.path.basename(po[:-3])
-            mo = os.path.join(MO_DIR, lang, '%s.mo' % APP_NAME)
+            mo = os.path.join(MO_DIR, lang, "%s.mo" % APP_NAME)
 
             directory = os.path.dirname(mo)
             if not os.path.exists(directory):
-                info('creating %s' % directory)
+                info("creating %s" % directory)
                 os.makedirs(directory)
 
             if newer(po, mo):
-                info('compiling %s -> %s' % (po, mo))
+                info("compiling %s -> %s" % (po, mo))
                 try:
-                    rc = subprocess.call(['msgfmt', '-o', mo, po])
+                    rc = subprocess.call(["msgfmt", "-o", mo, po])
                     if rc != 0:
                         raise Warning("msgfmt returned %d" % rc)
                 except Exception as e:
-                    error("Building gettext files failed.  Try setup.py \
-                        --without-gettext [build|install]")
+                    error(
+                        "Building gettext files failed.  Try setup.py \
+                        --without-gettext [build|install]"
+                    )
                     error("Error: %s" % str(e))
                     sys.exit(1)
+
+        # Run the standard build_py
+        build_py.run(self)
 
 
 class InstallData(install_data):
@@ -66,9 +80,9 @@ class InstallData(install_data):
     def _find_mo_files():
         data_files = []
 
-        for mo in glob.glob(os.path.join(MO_DIR, '*', '%s.mo' % APP_NAME)):
+        for mo in glob.glob(os.path.join(MO_DIR, "*", "%s.mo" % APP_NAME)):
             lang = os.path.basename(os.path.dirname(mo))
-            target = os.path.join('share', 'locale', lang, 'LC_MESSAGES')
+            target = os.path.join("share", "locale", lang, "LC_MESSAGES")
             data_files.append((target, [mo]))
 
         return data_files
@@ -79,38 +93,41 @@ class InstallData(install_data):
 
 
 setup(
-    name='migasfree-sdk',
+    name="migasfree-sdk",
     version=VERSION,
-    url='https://github.com/migasfree/migasfree-sdk/',
-    packages=['migasfree_sdk'],
+    url="https://github.com/migasfree/migasfree-sdk/",
+    packages=["migasfree_sdk"],
     data_files=[
-        ('share/doc/migasfree-sdk', [
-            'AUTHORS',
-            'INSTALL',
-            'LICENSE',
-            'README.md',
-            'VERSION',
-        ]),
+        (
+            "share/doc/migasfree-sdk",
+            [
+                "AUTHORS",
+                "INSTALL",
+                "LICENSE",
+                "README.md",
+                "VERSION",
+            ],
+        ),
     ],
     cmdclass={
-        'build': BuildData,
-        'install_data': InstallData,
+        "build_py": BuildData,
+        "install_data": InstallData,
     },
-    author='Jose Antonio Chavarría',
-    author_email='jachavar@gmail.com',
-    license='GPLv3',
-    platforms=['Linux'],
-    description='Migasfree Software Development Kit',
+    author="Jose Antonio Chavarría",
+    author_email="jachavar@gmail.com",
+    license="GPLv3",
+    platforms=["Linux"],
+    description="Migasfree Software Development Kit",
     long_description=README,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
     ],
 )
